0552b893fcb620469885545086751a9cca331b23
alex.wu <alex.wu@gcash.co>
2021-10-15 16:23:09 +0800

    fix: 🐛 wrong exception catch at filter
    Rpc exception cath also catch http exception, exclude it

--- src/filter/rpc.exception.filter.ts
+++ src/filter/rpc.exception.filter.ts
@@ -3,24 +3,28 @@ import {
   ExceptionFilter,
   ArgumentsHost,
   HttpStatus,
+  HttpException,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { status as RcpStatus } from '@grpc/grpc-js';
+import { RpcException } from '@nestjs/microservices';
 
-@Catch()
+@Catch(RpcException)
 export class RpcExceptionToHttpFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const httpStatus = RpcStatusCodeTranform(exception['code']);
+    if (exception instanceof HttpException) {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse<Response>();
+      const httpStatus = RpcStatusCodeTranform(exception['code']);
 
-    if (!httpStatus || httpStatus === HttpStatus.INTERNAL_SERVER_ERROR)
-      exception['details'] = 'Internal server error';
+      if (!httpStatus || httpStatus === HttpStatus.INTERNAL_SERVER_ERROR)
+        exception['details'] = 'Internal server error';
 
-    response.status(httpStatus).json({
-      status: httpStatus,
-      error: exception['details'],
-    });
+      response.status(httpStatus).json({
+        status: httpStatus,
+        error: exception['details'],
+      });
+    }
   }
 }
 function RpcStatusCodeTranform(rpcCode: RcpStatus): HttpStatus {
